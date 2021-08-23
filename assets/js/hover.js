c9bb8c13b4115d010aba56ea03b1a37200b21215
Jonathan-Ryndolf <ryndolf2001@gmail.com>
2021-08-23 22:36:21 +0530

    Made button disable

--- assets/js/hover.js
+++ assets/js/hover.js
@@ -2,12 +2,16 @@ function checker() {
   if (document.getElementById("checker").classList.contains("deactive")) {
     document.getElementById("checker").classList.remove("deactive");
   } else {
-    document.getElementById("checker").classList.add("deactive");
+    var checker = document.getElementById("checker");
+    checker.classList.add("deactive");
   }
-
   if (document.getElementById("clickbox").classList.contains("clickcolor")) {
     document.getElementById("clickbox").classList.remove("clickcolor");
+    var checker = document.getElementById("checker");
+    checker.removeAttribute("disabled");
   } else {
     document.getElementById("clickbox").classList.add("clickcolor");
+    var checker = document.getElementById("checker");
+    checker.setAttribute("disabled", true);
   }
 }
